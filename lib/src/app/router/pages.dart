04fea188b6bbfba31b30c85a1efa039ab2cf10cd
YAVILES <yonathanaviles123@gmail.com>
2022-06-24 15:40:50 -0400

    Update pages.dart

--- lib/src/app/router/pages.dart
+++ lib/src/app/router/pages.dart
@@ -17,9 +17,7 @@ abstract class AppPages {
       children: [
         GetPage(
           name: Routes.home,
-          page: () {
-            return const MyHomePage(title: "Home");
-          },
+          page: () => const MyHomePage(title: "Home"),
           transition: Transition.zoom,
         ),
         GetPage(
