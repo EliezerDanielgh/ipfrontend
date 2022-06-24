c2553ffa07dec4d0754520edf5477359af3f1cf7
YAVILES <yonathanaviles123@gmail.com>
2022-06-24 12:11:26 -0400

    Correcciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ipfrontend/src/app/controllers/auth_controller.dart';
+import 'package:ipfrontend/src/app/controllers/covex_controller.dart';
+import 'package:ipfrontend/src/app/controllers/nav_drawer_controller.dart';
 import 'package:ipfrontend/src/app/utils/apidio.dart';
 import 'package:ipfrontend/src/app/utils/preferences.dart';
 
@@ -7,6 +11,9 @@ import 'package:ipfrontend/src/app/app.dart';
 Future<void> main() async {
   await Preferences.configurePrefs();
   APIDio.configureDio();
+  Get.put(AuthController());
+  Get.put(ConvexController());
+  Get.put(NavDrawerController());
   runApp(const AppState());
 }
 
